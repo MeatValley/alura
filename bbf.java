a888ee20e308c0ed0ee7c496091e186ad7d6d4ec
MeatValley <81434373+MeatValley@users.noreply.github.com>
2024-01-25 08:47:49 -0300

    projeto inicial

---
+++ bbf.java
@@ -0,0 +1,20 @@
+public class bbf {
+    //github hospeda o codigo
+    //git eh controle de versoes
+    public static void main(String[] args) {
+        System.out.println("Principais comandos do Git:");
+
+        System.out.println("1. git init - Inicializa um repositório Git local");
+        System.out.println("2. git clone <URL do repositório> - Clona um repositório remoto");
+        System.out.println("3. git add <arquivo> - Adiciona alterações ao índice");
+        System.out.println("4. git commit -m 'mensagem' - Realiza um commit com as alterações no índice");
+        System.out.println("5. git status - Exibe o status das alterações");
+        System.out.println("6. git pull - Atualiza o repositório local com as alterações do remoto");
+        System.out.println("7. git push - Envia as alterações locais para o repositório remoto");
+        System.out.println("8. git branch - Lista as branches locais");
+        System.out.println("9. git checkout <nome da branch> - Muda para uma branch específica");
+        System.out.println("10. git merge <nome da branch> - Mescla uma branch na branch atual");
+
+        // Adicione mais comandos conforme necessário
+    }
+}
